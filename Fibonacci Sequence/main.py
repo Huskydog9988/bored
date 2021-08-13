5f617640abbdd569aa8063fde8a2b48db5a526a3
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-08-13 12:57:29 -0400

    refactor fibonacci to improve reusability

--- Fibonacci Sequence/main.py
+++ Fibonacci Sequence/main.py
@@ -1,18 +1,38 @@
-def Fibonacci(Start):
-    sequence = Start
+
+# computes result of n1 & n2
+def Finale(n1, n2):
+
+    # compute number
+    finale = n1+n2
+
+    return finale
+
+
+# computes a full Fibonacci Sequence
+def Fibonacci(start1, start2):
+    sequence = [start1, start2]
+
     while True:
+
         n1 = sequence[-1]
         n2 = sequence[-2]
-        finale = n1+n2
-        sequence.append(finale)
+
+        sequence.append(Finale(n1, n2))
+
+        # get final
+        finale = sequence[-1]
         print(finale)
+
+        # input("Press Enter to continue...")
+
 
 def main():
     # Ex: 0
     one = int(input("Starting number 1: "))
     # Ex: 1
     two = int(input("Starting number 2: "))
-    Fibonacci([one,two])
+
+    Fibonacci(one, two)
 
 
 if __name__ == "__main__":
